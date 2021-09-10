09f65f10fe1af0cf0efca32f022d69fc975c32fa
Arkadiy <arzumanov.exceedteam@gmail.com>
2021-09-10 11:04:53 +0300

    adding forms auth and regist

---
+++ src/components/Auth/Auth.js
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+import { Link } from 'react-router-dom';
+
+import './Auth.css'
+
+class Auth extends Component {
+    render() {
+        return (
+            <div className="form-body">
+                <h2 className='form-title'>sign in</h2>
+                <form className='form'>
+                    <div className='form__wrapper'>
+                        <label for='login_field'>
+                            Username
+                        </label>
+                        <input
+                            className='form__input'
+                            id='login_field'
+                            type='text'
+                            name='login'
+                        />
+
+                        <label for='pass_field'>
+                            Password
+                        </label>
+                        <input
+                            className='form__input'
+                            id='pass_field'
+                            type='password'
+                            name='password'
+                        />
+                        <button className='form__btn'>
+                            sign in
+                        </button>
+                    </div>
+                </form>
+
+                <div className='form-footer'>
+                    <p>
+                        New user? 
+                        <Link className='form-footer__link' to='/regist'> Create an account
+                        </Link>
+                    </p>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Auth;
---
+++ src/components/Regist/Regist.js
@@ -0,0 +1,61 @@
+import React, { Component } from "react";
+import { Link } from 'react-router-dom';
+
+import './Regist.css'
+
+class Regist extends Component {
+    render() {
+        return (
+            <div className="form-body">
+                <h2 className='form-title'>sign up</h2>
+                <form className='form'>
+                    <div className='form__wrapper'>
+
+                        <label for='email_field'>
+                            Enter your email
+                        </label>
+                        <input
+                            className='form__input'
+                            id='email_field'
+                            type='email'
+                            name='login'
+                        />
+
+                        <label for='pass_field'>
+                            Create a password
+                        </label>
+                        <input
+                            className='form__input'
+                            id='pass_field'
+                            type='password'
+                            name='password'
+                        />
+
+                        <label for='login_field'>
+                            Enter a username
+                        </label>
+                        <input
+                            className='form__input'
+                            id='login_field'
+                            type='text'
+                            name='login'
+                        />
+                        <button className='form__btn'>
+                            sign up
+                        </button>
+                    </div>
+                </form>
+
+                <div className='form-footer'>
+                    <p>
+                        You have an account?
+                        <Link className='form-footer__link' to='/auth'> Sign in
+                        </Link>
+                    </p>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Regist;
